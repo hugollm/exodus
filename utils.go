8628f80c24cff05865c7ce6260efc2c9e036f8bb
Hugo Mota <hugo.txt@gmail.com>
2017-05-31 22:07:55 -0300

    Add RandomFloat64 utility function

--- utils.go
+++ utils.go
@@ -5,3 +5,7 @@ import "math/rand"
 func RandomInt(min int, max int) int {
     return rand.Intn(max - min + 1) + min
 }
+
+func RandomFloat64(min float64, max float64) float64 {
+    return rand.Float64() * (max - min) + min
+}
--- utils_test.go
+++ utils_test.go
@@ -22,3 +22,24 @@ func TestRandomInt(t *testing.T) {
         }
     }
 }
+
+func TestRandomFloat64(t *testing.T) {
+    for i := 0; i < 1000; i++ {
+        n := RandomFloat64(0, 1)
+        if n < 0 || n > 1 {
+            t.Fail()
+        }
+    }
+    for i := 0; i < 1000; i++ {
+        n := RandomFloat64(0, 2)
+        if n < 0 || n > 2 {
+            t.Fail()
+        }
+    }
+    for i := 0; i < 1000; i++ {
+        n := RandomFloat64(1, 3)
+        if n < 1 || n > 3 {
+            t.Fail()
+        }
+    }
+}
