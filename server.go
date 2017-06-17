7cec3557ca42a5c3840a98589f06494ff28f2ea6
Hugo Mota <hugo.txt@gmail.com>
2017-06-16 21:27:30 -0300

    Refactor server code

--- server.go
+++ server.go
@@ -14,36 +14,35 @@ func InServer() bool {
 
 func RunServer(search *Search) {
     globalSearch = search
-    http.HandleFunc("/test-connection", testConnection)
-    http.HandleFunc("/send-best", sendBest)
-    http.HandleFunc("/migrate", migrate)
+    http.HandleFunc("/test-connection", TestConnectionEndpoint)
+    http.HandleFunc("/send-best", SendBestEndpoint)
+    http.HandleFunc("/migrate", MigrateEndpoint)
     err := http.ListenAndServe(":2345", nil)
     if err != nil {
         panic(err)
     }
 }
 
-func testConnection(response http.ResponseWriter, request *http.Request) {
+func TestConnectionEndpoint(response http.ResponseWriter, request *http.Request) {
     json.NewEncoder(response).Encode(true)
 }
 
-func sendBest(response http.ResponseWriter, request *http.Request) {
+func SendBestEndpoint(response http.ResponseWriter, request *http.Request) {
     individual := Individual{}
     body, _ := ioutil.ReadAll(request.Body)
     json.Unmarshal(body, &individual)
+    receiveBest(individual)
+}
+
+func receiveBest(individual Individual) {
     if globalSearch.Population.Best.Fitness < individual.Fitness {
         globalSearch.Population.Best = individual
         globalSearch.Best = individual
     }
 }
 
-func migrate(response http.ResponseWriter, request *http.Request) {
-    emigrant := Individual{}
-    if len(globalSearch.Imigrants) > 0 {
-        i := rand.Intn(len(globalSearch.Imigrants))
-        emigrant = globalSearch.Imigrants[i]
-        globalSearch.Imigrants = append(globalSearch.Imigrants[:i], globalSearch.Imigrants[i+1:]...)
-    }
+func MigrateEndpoint(response http.ResponseWriter, request *http.Request) {
+    emigrant := popImigrantFromSearch()
     json.NewEncoder(response).Encode(emigrant)
     imigrant := Individual{}
     body, _ := ioutil.ReadAll(request.Body)
@@ -53,12 +52,20 @@ func migrate(response http.ResponseWriter, request *http.Request) {
 
 func MigrateLocally() {
     if len(globalSearch.Imigrants) > 0 {
-        imigrantsIndex := rand.Intn(len(globalSearch.Imigrants))
-        emigrant := globalSearch.Imigrants[imigrantsIndex]
-        globalSearch.Imigrants = append(globalSearch.Imigrants[:imigrantsIndex], globalSearch.Imigrants[imigrantsIndex+1:]...)
-        populationIndex := rand.Intn(len(globalSearch.Population.Individuals))
-        globalSearch.Population.Individuals[populationIndex] = emigrant
+        imigrant := popImigrantFromSearch()
+        i := rand.Intn(len(globalSearch.Population.Individuals))
+        globalSearch.Population.Individuals[i] = imigrant
     }
-    imigrant := globalSearch.Population.SelectIndividual()
-    globalSearch.Imigrants = append(globalSearch.Imigrants, imigrant.CopyWithFitness())
+    emigrant := globalSearch.Population.SelectIndividual()
+    globalSearch.Imigrants = append(globalSearch.Imigrants, emigrant.CopyWithFitness())
+}
+
+func popImigrantFromSearch() Individual {
+    imigrant := Individual{}
+    if len(globalSearch.Imigrants) > 0 {
+        i := rand.Intn(len(globalSearch.Imigrants))
+        imigrant = globalSearch.Imigrants[i]
+        globalSearch.Imigrants = append(globalSearch.Imigrants[:i], globalSearch.Imigrants[i+1:]...)
+    }
+    return imigrant
 }
